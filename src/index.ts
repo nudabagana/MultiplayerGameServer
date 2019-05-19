f2d261bd8e51c2812fededfd9312741768add5c3
nudabagana <matasramanauskas@gmail.com>
2019-05-19 18:03:50 +0300

    ws added

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,53 @@
-import express from 'express';
+import express from "express";
 import * as dotenv from "dotenv";
-import * as os from 'os';
-import { createServer } from 'http';
-import * as path from 'path';
-import * as  gameLoop from 'node-gameloop';
+import * as os from "os";
+import { createServer } from "http";
+import * as path from "path";
+import * as gameLoop from "node-gameloop";
+import WebSocket from "ws";
+import { Socket } from "net";
 
 dotenv.config();
 const host = os.hostname();
-if (!process.env.PORT){
+if (!process.env.PORT) {
   throw new Error(`PORT not Found! Please set it in environment variables.`);
 }
 const port = process.env.PORT;
-const TICK_TIME = 1000/30;
+const TICK_TIME = 1000 / 30;
+const TICK_TIME_SECONDS = 1 / 30;
 let unsimulatedTime = 0;
 let currentTick = 0;
 
-// const app = express();
+const server = createServer();
 
-// app.use(express.static('dist'));
+const ws = new WebSocket.Server({ server });
 
-// const server = createServer(app);
+ws.on("connection", socket => {
+  console.log("NEW USER CONNECTED!");
 
-// app.get('/',(req : express.Request, res : express.Response) => {
-//   res.sendFile(path.join(__dirname + '/../client/index.html'));
-// });
+  socket.onmessage = message => console.log("received: %s", message);
 
-// app.get('/favicon.ico',(req : express.Request, res : express.Response) => {
-//   res.sendFile(path.join(__dirname + '/../assets/favicon.png'));
-// });
+  socket.onclose = () => console.log("User disconnected");
 
-// server.listen(port, () => {
-//   console.log(`Server ready http://${host}:${port}`);
-// });
+  socket.send("welcome to the game!");
+});
 
+server.listen(port, () => {
+  console.log(`Server ready http://${host}:${port}`);
+});
 
 const id = gameLoop.setGameLoop(function(delta) {
   unsimulatedTime += delta;
-  while (unsimulatedTime > TICK_TIME){
+
+  while (unsimulatedTime > TICK_TIME_SECONDS) {
     // Simulate physics step
-    unsimulatedTime -= TICK_TIME;
+    unsimulatedTime -= TICK_TIME_SECONDS;
     currentTick++;
   }
-  if (currentTick % 1000 === 0){
+  if (currentTick % 1000 === 0) {
     console.log(`Current server tick: ${currentTick}`);
   }
 }, TICK_TIME);
-
 
 const terminate = async () => {
   gameLoop.clearGameLoop(id);
@@ -53,5 +55,5 @@ const terminate = async () => {
   process.exit(1);
 };
 // Signals on witch we will what to close connection to service.
-process.on('SIGTERM', () => terminate().catch(err => console.log(err)));
-process.on('SIGINT', () => terminate().catch(err => console.log(err)));
+process.on("SIGTERM", () => terminate().catch(err => console.log(err)));
+process.on("SIGINT", () => terminate().catch(err => console.log(err)));
